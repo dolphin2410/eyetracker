6b94a3dcaea083053d4cd6774d174f1c76927bf4
dolphin2410 <dolphin2410@outlook.kr>
2025-12-29 23:32:24 +0900

    :hammer: Better history management system

--- util/eyetracker_history.py
+++ util/eyetracker_history.py
@@ -13,20 +13,36 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import numpy as np
+import json
+
+def parse_offset_from_vector(v):
+    v = np.array(v)
+
+    mean = np.mean(v)
+
+    return (v[1] - mean) / mean
+
 class EyetrackerHistoryFrame():
     """A wrapper class for faceparser keypoints"""
 
-    def __init__(self, keypoints):
-        self.keypoints = keypoints
+    def __init__(self, x_w, y_w, x_b, y_b):
+        self.raw_points_x = [x_w, x_b]
+        self.raw_points_y = [y_w, y_b]
+
+        self.norm_x = parse_offset_from_vector(self.raw_points_x)
+        self.norm_y = parse_offset_from_vector(self.raw_points_y)
 
 class EyetrackerHistory():
     """This class stores a time series of faceparser keypoints"""
     
     def __init__(self):
+        self.curr_frame = None
         self.frames = []
 
     def append_frame(self, frame: EyetrackerHistoryFrame):
         self.frames.append(frame)
 
-    def save_history(self):
-        pass # todo implement
+    def save_history(self, target_file):
+        with open(target_file, 'w') as wf:
+            json.dump(list(map(lambda x: x.__dict__, self.frames)), wf)
