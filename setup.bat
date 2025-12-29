342e5f50add73f97983c1e7db308d176be8b6ce4
dolphin2410 <dolphin2410@outlook.kr>
2025-12-30 00:20:47 +0900

    :hammer: Add setup script for windows

---
+++ setup.bat
@@ -0,0 +1,11 @@
+python -m venv eyetracker
+./eyetracker/bin/activate
+
+mv patches/face_parsing.patch face_parsing/patch.patch
+cd face_parsing
+git apply patch.patch
+cd ../
+pip install ./face_parsing
+pip install -r requirements.txt
+curl -L https://github.com/yakhyo/face-parsing/releases/download/weights/resnet34.pt -o face_parsing/weights/resnet34.pt
+curl -L https://github.com/YapaLab/yolo-face/releases/download/1.0.0/yolov12n-face.pt -o yolov12n-face.pt
