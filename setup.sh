d7f3e02647defb1c3138b530a456ea36d2f8efc1
dolphin2410 <dolphin2410@outlook.kr>
2025-12-30 00:14:32 +0900

    :hammer: Fix broken script

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ git apply patch.patch
 cd ../
 pip install ./face_parsing
 pip install -r requirements.txt
-curl -OL https://github.com/yakhyo/face-parsing/releases/download/weights/resnet34.pt -o face_parsing/weights/resnet34.pt
-curl -OL https://github.com/YapaLab/yolo-face/releases/download/v1.0.0/yolov12n-face.pt -o yolov12n-face.pt
+curl -L https://github.com/yakhyo/face-parsing/releases/download/weights/resnet34.pt -o face_parsing/weights/resnet34.pt
+curl -L https://github.com/YapaLab/yolo-face/releases/download/1.0.0/yolov12n-face.pt -o yolov12n-face.pt
